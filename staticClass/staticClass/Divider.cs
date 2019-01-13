4f474c358e0997f8778a6e2c09f8f55abc55e636
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-01-13 08:19:07 -0800

    StaticClass

---
+++ staticClass/staticClass/Divider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class_App
+{
+    class Divider
+    {
+        //Created "Divider" class. Created void "Half" method.
+        
+        public static void Half(out int dividedNum, int userInt)
+        {
+            dividedNum = userInt / 2;
+        }
+
+        //Made second method to overload "Half" method.
+        public static void Half(int userInt)
+        {
+            int dividedNum = userInt / 2;
+        }
+    }
+}
---
+++ staticClass/staticClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiated the "divider" object of the "Divider" class.
+            Divider divider = new Divider();
+
+            // Asked user for numeral, and called method on the numeral entered by the user.
+            Console.WriteLine("Please enter a numeral to be divided by 2.");
+
+            int userInt = Convert.ToInt32(Console.ReadLine());
+            int dividedNum = 0;
+
+            Divider.Half(out dividedNum, userInt);
+
+            Console.WriteLine(dividedNum);
+            Console.ReadLine();
+        }
+    }
+}
