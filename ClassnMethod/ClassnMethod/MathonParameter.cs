e0689f225db7d017567360ba4ff951f5f0c5e945
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-01-13 04:17:33 -0800

    classANDmethod

---
+++ ClassnMethod/ClassnMethod/MathonParameter.cs
@@ -0,0 +1,31 @@
+﻿using MethodsDrill;
+
+
+namespace MethodsDrill
+{
+    public class Math_ing
+    {
+        public int MutilplytwoValues(int y, int  x)
+        {
+            return y * x;
+        }
+        public int AddThreeValues(int x ,int y, int z)
+        {
+            return x + y + z;
+        }
+        public int AddNMultiplyFourValues(int x, int y, int z, int i)
+        {
+            return (x+ y)* ( z + i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ ClassnMethod/ClassnMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace MethodsDrill
+{
+    public class MethodsDrill
+    {
+        static void Main()
+        {
+            Console.WriteLine("Pick a number and it will Add your number to my Equations!");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Math_ing a = new Math_ing();
+            int x = a.MutilplytwoValues(5, 10);
+            int y = a.AddThreeValues(15, 25, 35);
+            int z = a.AddNMultiplyFourValues(10, 20, 30, 40);
+
+            Console.WriteLine(x + userInput);
+            Console.ReadLine();
+            Console.WriteLine(y + userInput);
+            Console.ReadLine();
+            Console.WriteLine(z + userInput);
+            Console.ReadLine();
+        }
+    }
+}
