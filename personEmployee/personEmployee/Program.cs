293c7ae2c9ba2a0cc0d1704fe7882e4fe781059a
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-01-13 09:28:16 -0800

    PersonEmployee

---
+++ personEmployee/personEmployee/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Employee_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiated "employee" object in "Employee" class with FirstName and LastName parameters.
+            Employee employee = new Employee { FirstName = "Sample", LastName = "Student" };
+
+            //Called the superclass ("Person") method SayName() on the "employee" object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
