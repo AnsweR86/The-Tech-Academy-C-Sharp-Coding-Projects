1605dbc26fa6551a778d0ee99d0f1237764a074e
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-01-13 05:10:06 -0800

    classNmethodNprameter

---
+++ classNmethodNparameter.cs/classNmethodNparameter.cs/Prameter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace MethodDrill2
+
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public int Multiplication (int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Subtraction(double num1, double num2)
+        {
+            return Convert.ToInt32(num1 - num2);
+        }
+
+        public int Addition(string one, string two)
+        {
+            int ex = 0;
+            int why = 0;
+            if (!Int32.TryParse(one, out ex))
+            {
+                ex= -1;
+            }
+            if (!Int32.TryParse(two, out why))
+            {
+                why= -1;
+            }
+            return ex + why;
+        }
+    }
+}
---
+++ classNmethodNparameter.cs/classNmethodNparameter.cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   ///Integer
+            Calculator calc = new Calculator();
+            int result = 0;
+            result = calc.Multiplication(12, 10);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            ///Decimal
+            Calculator calc2 = new Calculator();
+            int result2 = 0;
+            result2 = calc2.Subtraction(15, 2.2);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+            /// and STRING
+            Calculator calc3 = new Calculator();
+            int result3 = 0;
+            result3 = calc3.Addition("15", "5");
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+        }
+    }
+}
