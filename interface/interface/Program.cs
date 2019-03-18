b5a5d200cf9f1412a7f5450899059da3941f0771
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-03-18 16:04:58 -0700

    Interface

--- interface/interface/Program.cs
+++ interface/interface/Program.cs
@@ -1,22 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OverloadingOperator_App
+namespace Interface
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Employee emp1 = new Employee { id = 123 };
-            Employee emp2 = new Employee { id = 123 };
+    class Employee : Person, IQuittable
 
-            bool compare = emp1 != emp2;
-            Console.WriteLine("Are the two ids equal?" + compare);
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: [{FirstName} {LastName}]");
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Program Over, Exit.");
             Console.ReadLine();
         }
     }
-}
 
+}
