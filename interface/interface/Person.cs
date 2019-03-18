cedc7e3fdde760c8d9d57e2f0e098809badfeb60
AnsweR86 <41041085+AnsweR86@users.noreply.github.com>
2019-03-18 16:09:28 -0700

    Update person.cs

--- interface/interface/Person.cs
+++ interface/interface/Person.cs
@@ -1,16 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OverloadingOperator_App
+namespace Interface
 {
-    public  class Person
+    public abstract class Person
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public abstract void SayName();
 
-        //public abstract void SayName();
     }
 }
